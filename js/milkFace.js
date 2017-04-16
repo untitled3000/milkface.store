8d2d3110d24c621415147417e40e6ac0256081f6
Sean Goodrich <sean@smgoodrich.com>
2017-04-16 04:21:02 -0700

    remove unnecessary code

--- js/milkFace.js
+++ js/milkFace.js
@@ -2,7 +2,7 @@ function loadThreeJS() {
     var loader = new THREE.FileLoader();
     loader.load( 'app.json', function ( text ) {
 
-        var player = new APP.Player();
+        var player = new milkFacePlayer();
         player.load( JSON.parse( text ) );
         player.setSize( window.innerWidth, window.innerHeight );
         player.play();
@@ -11,16 +11,6 @@ function loadThreeJS() {
 
         window.addEventListener( 'resize', function () {
             player.setSize( window.innerWidth, window.innerHeight );
-        } );
-
-        if ( location.search === '?edit' ) {
-            var button = document.createElement( 'a' );
-            button.id = 'edit';
-            button.href = 'https://threejs.org/editor/#file=' + location.href.split( '/' ).slice( 0, - 1 ).join( '/' ) + '/app.json';
-            button.target = '_blank';
-            button.textContent = 'EDIT';
-            document.body.appendChild( button );
-        }
-
-    } );
+        });
+    });
 }
