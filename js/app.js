e41cae8d1bb041e4955b52bad6ea52134e94a908
Kyle Goodrich <untitled11888@yahoo.com>
2017-07-08 20:09:22 -0700

    use obj loader & loading manager instead of jsonloader

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-function milkFaceScene () {
+function milkFaceScene (object, texture) {
 
 		var $this = this;
 		var milkFaceObject;
 		var loader;
-		var camera, scene, renderer;
+		var camera, scene, light, renderer;
 		var width, height;
 		var controls;
 		var mouseX, mouseY;
@@ -11,41 +11,45 @@ function milkFaceScene () {
 		var defaultRotation = new THREE.Euler(degrees2Rad(-2), degrees2Rad(44), 0);
 
 		this.dom = document.createElement( 'div' );
-		this.dom.className = "milkFaceContainer"
+		this.dom.className = "milkFaceContainer";
 
-		this.load = function ( json ) {
-			loader = new THREE.ObjectLoader();
+		init (object, texture);
+
+		function init (object, texture) {
 
 			renderer = new THREE.WebGLRenderer( { antialias: true,  alpha: true  } );
 			renderer.setClearColor( 0xffffff, 0);
 			renderer.setPixelRatio( window.devicePixelRatio );
 
-			this.dom.appendChild( renderer.domElement );
+			$this.dom.appendChild( renderer.domElement );
 
-			setScene( loader.parse( json.scene ) );
-			setCamera( loader.parse( json.camera ) );
-
-			setMilkFace(scene.children[0].children[0]);
-			addBasicMaterial(milkFaceObject);
-			addNormalMap (milkFaceObject);
-			addEnvMap( milkFaceObject);
-			addDifMap (milkFaceObject);
+			setScene();
+			setLight();
+			setCamera();
+			setMilkFace(object, texture);
 
 			controls = new RotationWithQuaternion(milkFaceObject, camera);
 		};
 
-		function setCamera ( value ) {
+		function setCamera () {
 			camera = new THREE.PerspectiveCamera(50, (window.innerWidth / window.innerHeight), 0.1, 10000 );
 			camera.position.z = 9.5;
 			camera.updateProjectionMatrix();
 		};
 
-		function setScene ( value ) {
-			scene = value;
+		function setScene () {
+			scene = new THREE.Scene();
 		};
 
-		function setMilkFace ( value ) {
-			milkFaceObject = value;
+		function setLight () {
+			light = new THREE.HemisphereLight();
+			scene.add( light );
+		}
+
+		function setMilkFace ( object, texture ) {
+			milkFaceObject = object;
+			milkFaceObject.material = texture;
+			scene.add( milkFaceObject );
 		}
 
 		function setRotation (object, angle) {
@@ -57,50 +61,6 @@ function milkFaceScene () {
 		function degrees2Rad(degrees) {
 			return degrees * (Math.PI/180);
 		}
-
-		function addEnvMap ( object ){
-			var enviorment = new THREE.CubeTextureLoader()
-				.setPath( 'textures/lo_res/cube_map/' )
-				.load( [ 'right.png', 'left.png', 'top.png', 'bottom.png', 'front.png', 'back.png' ] );
-
-			object.material.envMap = enviorment;
-		};
-
-		function addDifMap (object){
-			var difMap = new THREE.TextureLoader().load( "./textures/lo_res/ao.png" );
-
-			object.material.map = difMap;
-		};
-
-		function addNormalMap (object){
-			var normMap = new THREE.TextureLoader().load( "./textures/lo_res/normal.png" );
-			var normScale = new THREE.Vector3( 1.0, 1.0, 1.0 );
-
-			object.material.normalMap = normMap;
-			object.material.normalScale = normScale;
-		};
-
-		function addBasicMaterial (object) {
-			var basicMaterial = new THREE.MeshStandardMaterial
-
-			({
-				"color": 15921906,
-				"roughness": 0.25,
-				"metalness": 0.05,
-				"emissive": 0,
-				"map": null,
-				"normalMap": null,
-				"normalScale": null,
-				"envMap": null,
-				"depthFunc": 3,
-				"depthTest": true,
-				"depthWrite": true,
-				"skinning": false,
-				"morphTargets": false
-			});
-
-			object.material = basicMaterial;
-		};
 
 		function rotateObject2Mouse ( event, object, xMag, yMag ) {
 			mouseX = event.clientX;
